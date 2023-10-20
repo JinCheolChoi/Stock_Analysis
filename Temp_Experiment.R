97f284cf445c85df3bc324f07367a024ad6e317f
Laptop <eor99999@gmail.com>
2023-10-20 15:49:14 -0700

    Temp_Experiment.R

--- Temp_Experiment.R
+++ Temp_Experiment.R
@@ -1,5 +1,85 @@
-RSI_Threshold=seq(10, 90, by=5)
-Consec_N=c(1:10)
+#**************
+# save and load
+#**************
+# save.image(paste0(rdata.dir, "Futures_", as.Date(Sys.time()), " - ", BarSize, ".Rdata"))
+# load(paste0(rdata.dir, "Futures_", as.Date(Sys.time()), " - ", BarSize, ".Rdata"))
+# load(paste0(rdata.dir, "Futures_2023-07-08 - 5mins.Rdata"))
+
+#********************
+#
+# empty the workspace
+#
+#********************
+rm(list=ls())
+
+
+#***********
+#
+# parameters
+#
+#******************
+# working directory
+#******************
+Device="laptop" # "laptop" or "desktop"
+
+if(Device=="desktop"){
+  # desktop
+  working.dir="C:/Users/JinCheol Choi/Desktop/R/Stock_Analysis/"
+  data.dir="E:/Stock_Data/" # upper folder that has a folder storing stock data
+  rdata.dir="C:/Users/JinCheol Choi/Desktop/R/Stock_Analysis_Daily_Data/Rdata/"
+  
+  source(paste0("C:/Users/JinCheol Choi/Desktop/R/Functions/Functions.R")) # desktop
+}else if(Device=="laptop"){
+  # laptop
+  working.dir="C:/Users/jchoi02/Desktop/R/Stock_Analysis/"
+  data.dir="C:/Users/jchoi02/Desktop/Data/" # upper folder that has a folder storing stock data
+  rdata.dir="C:/Users/jchoi02/Desktop/R/Stock_Analysis_Daily_Data/Rdata/"
+  
+  source(paste0("C:/Users/jchoi02/Desktop/R/Functions/Functions.R"))
+}
+
+
+#****************
+# data parameters
+#****************
+Symbols=c("MNQ")
+
+#*****************
+#
+# preliminary step
+#
+#*******************
+# load functions
+source(paste0(working.dir, "0. Stock_Analysis_Functions.R"))
+
+# import libraries
+for(pack in c("IBrokers",
+              "TTR",
+              "data.table",
+              "dplyr",
+              "DescTools", # candle chart
+              
+              "RcppRoll",
+              "Rcpp",
+              "RcppArmadillo",
+              "bench",
+              "tseries")){ 
+  lapply(pack, checkpackages)
+}
+
+# bar size
+BarSize="1min"
+
+# import data
+BarData=fread(paste0(data.dir, BarSize, "/", Symbols, "/", Symbols, ".csv"))
+BarData[, Time:=as.POSIXct(format(as.POSIXct(Time), tz="America/Los_Angeles"),
+                           tz="America/Los_Angeles")]
+
+# RSI_Threshold=seq(10, 90, by=5)
+# Consec_N=c(1:10)
+RSI_Threshold=c(30, 70)
+Consec_N=c(1, 2)
+
 Market_Open_Time="05:30:00"
 Market_Close_Time="14:00:00"
 
@@ -11,195 +91,165 @@ Output=data.table(expand.grid(
   N=NaN
 ))
 
-BarData_Temp=copy(BarData)
 Output[, Ind:=.I]
 
-Trading_Dates=unique(as.Date(BarData_Temp$Time))
-BarData_Temp=do.call(
-  rbind,
-  lapply(Trading_Dates,
-         #x=Trading_Dates[3]
-         function(x){
-           x=as.Date(x)
-           
-           Temp=BarData_Temp[Time>=paste0(x-1, " ", Market_Close_Time)&
-                               Time<paste0(x, " ", Market_Close_Time), ]
-           Temp=Temp[!(Time>=paste0(x, " ", Market_Open_Time)&
-                         Time<paste0(x, " ", Market_Close_Time)), ]
-           
-           if(nrow(Temp)>0){
-             Temp[, RSI_:=RSI(Close)]
-           }else{
-             NULL
-           }
-         })
-)
+Trading_Dates=unique(as.Date(BarData$Time))
 
-BarData_Temp[, Ind:=.I]
-# compute informative quantities
+# Time_Unit
+Time_Unit=BarData$Time[2]-BarData$Time[1]
+
+
+BarData_to_Use=BarData[
+  data.table(
+    Symbol=Symbols[1],
+    Time=seq(min(BarData$Time),
+             max(BarData$Time),
+             by=BarData$Time[2]-BarData$Time[1])
+  ),
+  on=c("Symbol", "Time")]
+BarData_to_Use[, Ind:=.I]
+
+# compute quantities
 for(ind in 1:nrow(Output)){
   
-  if(Output[ind, RSI_Threshold]<50){
-    BarData_Temp[, RSI_Bi:=(RSI_<Output$RSI_Threshold[ind])]
-    BarData_Temp[, RSI_Signal:=c(rep(NA, length=Output$Consec_N[ind]-1),
-                                 RcppRoll::roll_sum(RSI_Bi, Output$Consec_N[ind]))==Output$Consec_N[ind]]
+  assign(
+    paste0("BarData_Temp_", ind),
     
-    BarData_Temp[, Lagged_Close:=lag(Close)]
-    BarData_Temp[, Close_Diff:=Close-Open]
-    BarData_Temp[, High_Diff:=High-Open]
-    BarData_Temp[, Low_Diff:=Low-Open]
-    
-    BarData_Temp[, Lagged_Open:=shift(Open, -1)]
-    BarData_Temp[, Open_Diff:=Lagged_Open-Open]
-    
-    # first observation among consecutive observations that meet the criterion
-    Temp=BarData_Temp[RSI_Signal==TRUE &
+    do.call(
+      rbind,
+      lapply(Trading_Dates,
+             #x=Trading_Dates[2]
+             function(x){
+               x=as.Date(x)
+               
+               # consider the pre-market trading time
+               Temp=BarData[Time>=paste0(x-1, " ", Market_Close_Time)&
+                              Time<paste0(x, " ", Market_Close_Time), ]
+               Temp=Temp[!(Time>=paste0(x, " ", Market_Open_Time)&
+                             Time<paste0(x, " ", Market_Close_Time)), ]
+               
+               # compute informative quantities
+               if(nrow(Temp)>0){
+                 Temp[, RSI_:=RSI(Close)]
+                 
+                 if(Output[ind, RSI_Threshold]<50){
+                   Temp[, RSI_Bi:=(RSI_<Output$RSI_Threshold[ind])]
+                   Temp[, RSI_Signal:=c(rep(NA, length=Output$Consec_N[ind]-1),
+                                        RcppRoll::roll_sum(RSI_Bi, Output$Consec_N[ind]))==Output$Consec_N[ind]]
+                   
+                   # Temp[, Lagged_Close:=lag(Close)]
+                   # Temp[, Close_Diff:=Close-Open]
+                   # Temp[, High_Diff:=High-Open]
+                   # Temp[, Low_Diff:=Low-Open]
+                   # 
+                   # Temp[, Shifted_Open:=shift(Open, -1)]
+                   # Temp[, Open_Diff:=Shifted_Open-Open]
+                 }else if(Output[ind, RSI_Threshold]>=50){
+                   Temp[, RSI_Bi:=(RSI_>=Output$RSI_Threshold[ind])]
+                   Temp[, RSI_Signal:=c(rep(NA, length=Output$Consec_N[ind]-1),
+                                        RcppRoll::roll_sum(RSI_Bi, Output$Consec_N[ind]))==Output$Consec_N[ind]]
+                   
+                   # Temp[, Lagged_Close:=lag(Close)]
+                   # Temp[, Close_Diff:=Close-Open]
+                   # Temp[, High_Diff:=High-Open]
+                   # Temp[, Low_Diff:=Low-Open]
+                   # 
+                   # Temp[, Shifted_Open:=shift(Open, -1)]
+                   # Temp[, Open_Diff:=Shifted_Open-Open]
+                 }
+                 
+                 # first observation among consecutive observations that meet the criterion
+                 Temp[RSI_Signal==TRUE &
                         frollapply(frollapply(RSI_Signal, 2, sum, align="right"),
                                    2, sum, align="right")==1, ]
+               }else{
+                 NULL
+               }
+             })
+    )
     
-    # Percentage of RSI_Signal==TRUE
-    Output[ind,
-           Percentage:=mean(Temp[RSI_Signal==TRUE,
-                                 Close_Diff]<0)]
-    
-    # Mean
-    Output[ind,
-           Mean:=mean(Temp[RSI_Signal==TRUE,
-                           Close_Diff])]
-    
-    # N
-    Output[ind,
-           N:=nrow(Temp[RSI_Signal==TRUE, ])]
-  }else if(Output[ind, RSI_Threshold]>=50){
-    BarData_Temp[, RSI_Bi:=(RSI_>=Output$RSI_Threshold[ind])]
-    BarData_Temp[, RSI_Signal:=c(rep(NA, length=Output$Consec_N[ind]-1),
-                                 RcppRoll::roll_sum(RSI_Bi, Output$Consec_N[ind]))==Output$Consec_N[ind]]
-    
-    BarData_Temp[, Lagged_Close:=lag(Close)]
-    BarData_Temp[, Close_Diff:=Close-Open]
-    BarData_Temp[, High_Diff:=High-Open]
-    BarData_Temp[, Low_Diff:=Low-Open]
-    
-    BarData_Temp[, Lagged_Open:=shift(Open, -1)]
-    BarData_Temp[, Open_Diff:=Lagged_Open-Open]
-    
-    # first observation among consecutive observations that meet the criterion
-    Temp=BarData_Temp[RSI_Signal==TRUE &
-                        frollapply(frollapply(RSI_Signal, 2, sum, align="right"),
-                                   2, sum, align="right")==1, ]
-    
-    
-    # Percentage
-    Output[ind,
-           Percentage:=mean(Temp[RSI_Signal==TRUE,
-                                 Close_Diff]>=0)]
-    
-    # Mean
-    Output[ind,
-           Mean:=mean(Temp[RSI_Signal==TRUE,
-                           Close_Diff])]
-    
-    # N
-    Output[ind,
-           N:=nrow(Temp[RSI_Signal==TRUE, ])]
-  }
+  )
+  
+}
+
+# Ind
+for(ind in 1:nrow(Output)){
+  Temp=get(
+    paste0("BarData_Temp_", ind)
+  )
+  
+  assign(
+    paste0("BarData_Temp_", ind),
+    BarData_to_Use[, .SD, .SDcols=c("Symbol",
+                                    "Time",
+                                    "Ind")][Temp, on=c("Symbol", "Time")]
+  )
 }
 
 
-library(plotly)
-Output[is.na(Percentage), Percentage:=0]
-Output[is.na(Mean), Mean:=0]
-plot_ly(Output,
-        x = ~RSI_Threshold,
-        y = ~Consec_N, z = ~Percentage,
-        color=~N)
-
-
-
-Output[RSI_Threshold==30 &
-         Consec_N==1, ]
-
-#
-RSI_Threshold=30
-Consec_N=1
-
-if(RSI_Threshold<50){
-  BarData_Temp[, RSI_Bi:=(RSI_<RSI_Threshold)]
-}else{
-  BarData_Temp[, RSI_Bi:=(RSI_>=RSI_Threshold)]
+# compute informative quantities
+for(ind in 1:nrow(Output)){
+  Temp=get(
+    paste0("BarData_Temp_", ind)
+  )
+  
+  # if(Output[ind, RSI_Threshold]<50){
+  #   # Percentage of Close_Diff<0
+  #   Output[ind,
+  #          Percentage:=mean(Temp[RSI_Signal==TRUE,
+  #                                Close_Diff<0])]
+  #   
+  # }else if(Output[ind, RSI_Threshold]>=50){
+  #   # Percentage of Close_Diff>=0
+  #   Output[ind,
+  #          Percentage:=mean(Temp[RSI_Signal==TRUE,
+  #                                Close_Diff>=0])]
+  # }
+  # 
+  # # Mean
+  # Output[ind,
+  #        Mean:=mean(Temp[RSI_Signal==TRUE,
+  #                        Close_Diff])]
+  
+  # N
+  Output[ind,
+         N:=nrow(Temp)]
 }
-BarData_Temp[, RSI_Signal:=c(rep(NA, length=Consec_N-1),
-                             RcppRoll::roll_sum(RSI_Bi, Consec_N))==Consec_N]
-
-BarData_Temp[, Lagged_Close:=lag(Close)]
-BarData_Temp[, Close_Diff:=Close-Open]
-BarData_Temp[, High_Diff:=High-Open]
-BarData_Temp[, Low_Diff:=Low-Open]
 
 
-# first observation among consecutive observations that meet the criteria
-Temp=BarData_Temp[RSI_Signal==TRUE &
-                    frollapply(frollapply(RSI_Signal, 2, sum, align="right"),
-                               2, sum, align="right")==1, ]
-
-nrow(Temp[RSI_Signal==TRUE, ])
-
-#
-mean(Temp[RSI_Signal==TRUE,
-          Close_Diff])
-
-hist(Temp[RSI_Signal==TRUE &
-            Close_Diff> -100,
-          Close_Diff],
-     breaks=100)
-hist(Temp[RSI_Signal==TRUE &
-            Close_Diff> -100,
-          High_Diff],
-     breaks=50)
-hist(Temp[RSI_Signal==TRUE &
-            Close_Diff> -100,
-          Low_Diff],
-     breaks=50)
+# # 3D plot
+# library(plotly)
+# Output[is.na(Percentage), Percentage:=0]
+# Output[is.na(Mean), Mean:=0]
+# plot_ly(Output,
+#         x = ~RSI_Threshold,
+#         y = ~Consec_N, z = ~Percentage,
+#         color=~N)
+# 
+# Output[RSI_Threshold==30 &
+#          Consec_N==1, ]
 
 
+for(ind in 1:nrow(Output)){
+  assign(
+    paste0("BarData_Temp_", ind),
+    Value_Difference_Normalizer(get(paste0("BarData_Temp_", ind)),
+                                Width=30)
+  )
+  # summary(Temp[Order_Ind==10, Normalized_Close])
+  plot(get(paste0("BarData_Temp_", ind))[, mean(Normalized_Close), by="Order_Ind"])
+}
 
-Temp_2=do.call(rbind,
-               lapply(0:10,
-                      function(x){
-                        Temp_2=BarData_Temp[Ind%in%(Temp$Ind+x)]
-                        
-                        Temp_2[, Order_Ind:=x]
-                      }))
-
-Temp_2[, Normalized_Close:=unlist(lapply(0:10,
-                                         function(x){
-                                           mapply(
-                                             function(a, b){
-                                               b-a
-                                             },
-                                             BarData_Temp[Ind%in%(Temp$Ind), Close],
-                                             BarData_Temp[Ind%in%(Temp$Ind+x), Close]
-                                           )
-                                         }))]
+BarData_Temp_1[Order_Ind==0|
+                 Order_Ind==1, ]
 
 
-Temp_2[Order_Ind==0, ] # 12976.75
-Temp_2[Order_Ind==1, ] # 12977.75
-Temp_2[Order_Ind==2, ] # 12979.75
-
-plot(Temp_2$Order_Ind, Temp_2$Normalized_Close)
-boxplot(Normalized_Close~Order_Ind,
-        Temp_2)
-plot(Temp_2[, mean(Normalized_Close), by="Order_Ind"])
-plot(Temp_2[, median(Normalized_Close), by="Order_Ind"])
+sapply(c(1:10),
+       function(x){
+         sum([order(Ind)][Order_Ind==x, Normalized_Close])
+       })
 
 
-rq(
-  Normalized_Close~Order_Ind,
-  data=Temp_2
-)
+# plot(Temp[, median(Normalized_Close), by="Order_Ind"])
 
-lm(
-  Normalized_Close~Order_Ind,
-  data=Temp_2
-)
+
